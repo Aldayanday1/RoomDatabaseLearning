6c9998c00646aff4a43a22301eca25c59d82ed7e
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-03 18:47:01 +0700

    Add object PenyediaViewModel  with the val Factory & viewModelFactory in it

--- app/src/main/java/project/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/project/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,10 @@
 package project.roomsiswa.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+    }
+}
