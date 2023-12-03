e8805a5779091d6fc7e15c83c8d8a603c84c7229
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-03 18:44:05 +0700

    Add class AplikasiSiswa for the 'Application' with lateinit var container

--- app/src/main/java/project/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/project/roomsiswa/AplikasiSiswa.kt
@@ -1,4 +1,11 @@
 package project.roomsiswa
 
-class AplikasiSiswa {
+import android.app.Application
+import project.roomsiswa.repositori.ContainerApp
+
+class AplikasiSiswa : Application() {
+    /*
+    * AppContainer instance digunakan oleh kelas-kelas lainnya untuk medapatkan dependensi
+    */
+    lateinit var container: ContainerApp
 }
