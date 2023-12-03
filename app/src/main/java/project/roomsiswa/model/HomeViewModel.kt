2c39315505ae6efce07fbe9370606f7a00c1d09b
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-03 18:31:35 +0700

    Add companion object for val with the name 'TIMEOUT_MILLIS' and the value '5_000L'

--- app/src/main/java/project/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/project/roomsiswa/model/HomeViewModel.kt
@@ -4,5 +4,7 @@ import androidx.lifecycle.ViewModel
 import project.roomsiswa.repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa):ViewModel(){
-
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
