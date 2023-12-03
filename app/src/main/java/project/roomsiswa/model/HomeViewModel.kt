bccfb6ae37c2f84e345bf32552141242f05ef205
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-03 18:26:47 +0700

    Add Class HomeViewModel with the private val & ViewModel in it

--- app/src/main/java/project/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/project/roomsiswa/model/HomeViewModel.kt
@@ -1,2 +1,8 @@
 package project.roomsiswa.model
 
+import androidx.lifecycle.ViewModel
+import project.roomsiswa.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa):ViewModel(){
+
+}
