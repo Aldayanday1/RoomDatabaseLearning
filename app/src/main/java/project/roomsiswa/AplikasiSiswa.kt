be902767e05f0804dd112c45f8646b7ced4687c8
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-03 18:45:34 +0700

    Add override fun onCreate with the 'super.onCreate' in it

--- app/src/main/java/project/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/project/roomsiswa/AplikasiSiswa.kt
@@ -2,10 +2,16 @@ package project.roomsiswa
 
 import android.app.Application
 import project.roomsiswa.repositori.ContainerApp
+import project.roomsiswa.repositori.ContainerDataApp
 
 class AplikasiSiswa : Application() {
     /*
     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk medapatkan dependensi
     */
     lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
