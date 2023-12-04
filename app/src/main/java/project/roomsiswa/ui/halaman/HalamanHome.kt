e8eb74dc3cdffdd4819428d269f19fe1236f8d60
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 18:56:28 +0700

    Add object DestinasiHome with 2 override val route & titleRes

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package project.roomsiswa.ui.halaman
 
+import project.roomsiswa.R
+import project.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
