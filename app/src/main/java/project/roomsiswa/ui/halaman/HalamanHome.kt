47bb1a49e97e3435562a7f1385366c5f2ac56128
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 18:59:35 +0700

    Add fun HomeScreen with OptIn and Composable in it

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
@@ -1,9 +1,25 @@
 package project.roomsiswa.ui.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import project.roomsiswa.R
+import project.roomsiswa.model.HomeViewModel
+import project.roomsiswa.model.PenyediaViewModel
 import project.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi{
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
