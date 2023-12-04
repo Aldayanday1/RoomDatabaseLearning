0d416563f9ae4891e2d0b77275010fa54cecd6ca
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 19:07:45 +0700

    Add Floating Action Button in Scaffold of HomeScreen

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
@@ -1,9 +1,21 @@
 package project.roomsiswa.ui.halaman
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import project.roomsiswa.R
 import project.roomsiswa.model.HomeViewModel
@@ -24,5 +36,28 @@ fun HomeScreen(
 ){
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+           FloatingActionButton(
+               onClick = navigateToItemEntry,
+               shape = MaterialTheme.shapes.medium,
+               modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_Large))
+           ){
+               Icon(
+                   imageVector = Icons.Default.Add,
+                   contentDescription = stringResource(R.string.entry_siswa)
+               )
+           }
+        },
+    ){
 
+    }
 }
