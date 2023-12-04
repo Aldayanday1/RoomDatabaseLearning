bc24ece999caffc0537146adc05274cab2e65099
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 19:25:52 +0700

    Add Layout LazyColumn of ListSiswa

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanHome.kt
@@ -3,6 +3,8 @@ package project.roomsiswa.ui.halaman
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -104,6 +106,18 @@ fun ListSiswa(
     itemSiswa: List<Siswa>,
     modifier: Modifier= Modifier
 ){
-
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+                person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
 }
+
+
+
 
