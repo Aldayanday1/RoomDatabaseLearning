0427ba5414aba32acc74cf8ccf0bd71e92d21eee
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 17:51:45 +0700

    Add Scaffold from the function of EntrySiswaScreen

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,14 +1,18 @@
 package project.roomsiswa.ui.halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import project.roomsiswa.R
 import project.roomsiswa.model.EntryViewModel
 import project.roomsiswa.model.PenyediaViewModel
+import project.roomsiswa.ui.halaman.DestinasiEntry.titleRes
 
 object DestinasiEntry: DestinasiEntry {
     override val route = "item_entry"
@@ -24,5 +28,16 @@ fun EntrySiswaScreen(
 ){
     val coroutinScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry,titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ){
 
+    }
 }
