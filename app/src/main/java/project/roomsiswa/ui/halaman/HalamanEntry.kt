8a35ddf21b83e7f28445748376549ce106e5ab24
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 17:40:58 +0700

    Add new object DestinasiEntry with the override val 'route' & 'titleRes'

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,2 +1,8 @@
 package project.roomsiswa.ui.halaman
 
+import project.roomsiswa.R
+
+object DestinasiEntry: DestinasiEntry {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
