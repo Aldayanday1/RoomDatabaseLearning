7b8d7504f247953cbfc65298e47fdc73832f9d78
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 17:43:38 +0700

    Add OptIn and Composable in HalamanEntry

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,8 +1,13 @@
 package project.roomsiswa.ui.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 import project.roomsiswa.R
 
 object DestinasiEntry: DestinasiEntry {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
