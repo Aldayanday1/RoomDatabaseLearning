69fcc267e04f67497a9b61c297043ff6a9efd88c
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 21:02:37 +0700

    Add function SiswaApp

--- app/src/main/java/project/roomsiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/project/roomsiswa/navigasi/PetaNavigasi.kt
@@ -1,2 +1,11 @@
 package project.roomsiswa.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
