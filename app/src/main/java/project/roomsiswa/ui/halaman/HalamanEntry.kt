fb5d23e4984ffa9868cc77c45ba41549c80ce60e
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 17:46:23 +0700

    Add 2 val of coroutinScope and scrollBehavior

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,7 +1,9 @@
 package project.roomsiswa.ui.halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import project.roomsiswa.R
@@ -20,5 +22,7 @@ fun EntrySiswaScreen(
     modifier: Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val coroutinScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
 }
