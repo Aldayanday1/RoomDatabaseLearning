fc01d6a621cdcc211c251726f42ddb75a4f35927
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-04 17:58:17 +0700

    Create modifier in EntrySiswaBody

--- app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/project/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,5 +1,9 @@
 package project.roomsiswa.ui.halaman
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -47,7 +51,11 @@ fun EntrySiswaScreen(
                     viewModel.saveSiswa()
                     navigateBack()
                 }
-            }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
         )
     }
 }
